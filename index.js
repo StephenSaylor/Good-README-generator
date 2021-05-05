96a6c4053fdacfcce6ef124ea50ec215f7b7c25f
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-05 12:40:17 -0400

    added prompt questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -35,6 +36,22 @@ const questions = [
 	},
 	
 ];
+
+const askQuestions = (questions) => {
+	inquirer	
+		.prompt(questions)
+		.then(
+			({
+				title,
+				description,
+				installation,
+				usage,
+				username,
+				licenses,				
+
+			})
+		)
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
