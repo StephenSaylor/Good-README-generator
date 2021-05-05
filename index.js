18ed19741406bfdc8e57be95cc8cfd3bdd6dc452
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-05 16:42:49 -0400

    readme template in generate markdown js

--- index.js
+++ index.js
@@ -75,9 +75,6 @@ err ? console.log(err) : console.log('Success!'))
 		)
 }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 function init() {
 
